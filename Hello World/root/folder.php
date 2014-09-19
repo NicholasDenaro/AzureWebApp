2f439f49ab8ce97e450453c49a0248ad02f7b86c
Nicholas Denaro <NicholasCDenaro@aol.com>
2014-09-19 16:35:54 -0400

    Added a ) to fix the PHP, I'm so silly.

--- Hello World/root/folder.php
+++ Hello World/root/folder.php
@@ -6,9 +6,12 @@ $ret="";
 
 foreach($results as $result)
 {
-	if($result==='./' or result==='../')continue;
+	if($result==='./' or result==='../')
+	{
+		continue;
+	}
 	
-	if(is_dir($path.$result)
+	if(is_dir($path.$result))
 	{
 		//some sort of return for it is a path;
 		$ret=$ret."|".$path.$result;
